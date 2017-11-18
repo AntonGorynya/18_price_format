5fcc0005d68d88c3f13239e7c8ca336d7867f488
Gorynya Anton <g84086619@china.huawei.com>
2017-11-18 19:48:48 +0300

    add new vars

--- format_price.py
+++ format_price.py
@@ -4,21 +4,23 @@ import re
 
 def format_price(price):
     price = str(price)
+    step = 3
+    fractal_len = 2
     point_position = -1
     if re.search(r'\S*([,\.])\S*', price):
-        point_position = re.search(r'\S*([,\.])\S*', price).start(1)
+        point_position = re.search(r'\S*(?P<pp>[,\.])\S*', price).start('pp')
     if point_position+1:
         whole_part = price[:point_position]
-        frac_part = price[point_position + 1:][:2].rstrip('0')
+        frac_part = price[point_position + 1:][:fractal_len].rstrip('0')
     else:
         whole_part = price
         frac_part = None
     out_price = ''
     for position, symbol in enumerate(whole_part[::-1]):
-        if position % 3 != 0:
+        if position % step != 0:
             out_price += symbol
         else:
-            out_price += ' '+symbol
+            out_price += ' {}'.format(symbol)
     whole_part = out_price[::-1].strip()
     if frac_part:
         return '{}.{}'.format(whole_part, frac_part)
