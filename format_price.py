b9db94c4a89f2d32eb25aee91b5ab114a3f1b412
Anton <antongorynya1@gmail.com>
2017-11-30 05:17:11 -0800

    rewrite function

--- format_price.py
+++ format_price.py
@@ -5,29 +5,14 @@ import re
 
 def format_price(price):
     price = str(price)
-    step = 3
-    fractal_len = 2
-    start_position = 2
     point_position = -1
     if re.search(r'\S*([,\.])\S*', price):
         point_position = re.search(r'\S*(?P<pp>[,\.])\S*', price).start('pp')
     if point_position+1:
-        whole_part = price[:point_position]
-        frac_part = price[point_position + 1:][:fractal_len].rstrip('0')
-    else:
-        whole_part = price
-        frac_part = None
-    whole_part = list(whole_part)[::-1]
-    for position in itertools.islice(itertools.count(),
-                                     start_position,
-                                     len(whole_part)-1,
-                                     step):
-        whole_part[position] = ' {}'.format(whole_part[position])
-    whole_part = ''.join(whole_part[::-1])
-    if frac_part:
-        return '{}.{}'.format(whole_part, frac_part)
-    else:
-        return whole_part
+        price = float(price.replace(',', '.'))
+    price = float(price)
+    price = '{:,.2f}'.format(price).replace(',', ' ').replace('.00', '')
+    return price
 
 
 def create_parser():
--- tests.py
+++ tests.py
@@ -8,16 +8,40 @@ class PriceTest(unittest.TestCase):
         self.assertEqual(price, '1 234 567')
 
     def test_float(self):
-        price = format_price(1234567.89)
-        self.assertEqual(price, '1 234 567.89')
-
-    def test_longfloat(self):
-        price = format_price(1234567.00089)
+        price = format_price(1234567.0000)
         self.assertEqual(price, '1 234 567')
 
+    def test_float_2(self):
+        price = format_price(12345.0300)
+        self.assertEqual(price, '12 345.03')
+
     def test_comma(self):
-        price = format_price('1234,55')
-        self.assertEqual(price, '1 234.55')
+        price = format_price('1234,0000')
+        self.assertEqual(price, '1 234')
+
+    def test_comma_2(self):
+        price = format_price('12345,0300')
+        self.assertEqual(price, '12 345.03')
+
+    def test_zero(self):
+        price = format_price('0,00')
+        self.assertEqual(price, '0')
+
+    def test_zero_2(self):
+        price = format_price('0,0300')
+        self.assertEqual(price, '0.03')
+
+    def test_negative(self):
+        price = format_price('-1234,0700')
+        self.assertEqual(price, '-1 234.07')
+
+    def test_point_begin(self):
+        price = format_price(',0700')
+        self.assertEqual(price, '0.07')
+
+    def test_point_end(self):
+        price = format_price('1,')
+        self.assertEqual(price, '1')
 
 
 if __name__ == '__main__':
